a5cbb71a4931c7c3a1fdecb5a0ddc00b07321795
lucasf14 <lucassky14@gmail.com>
2024-02-01 11:15:47 +0000

    Refactor synchronization to avoid recreating the whole replica folder

--- folder_sync.py
+++ folder_sync.py
@@ -2,34 +2,81 @@ import os
 import shutil
 import sys
 import time
+import logging
+
+
+def logger_setup(log_path):
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s | %(levelname)s | %(message)s',
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(log_path)
+        ]
+    )
 
 
 def synchronize_folders(source_folder, replica_folder):
     try:
         # Creates the directory and copies its content, if it doesn't exist
         if not os.path.exists(replica_folder):
-            shutil.copytree(source_folder, replica_folder)
+            os.makedirs(replica_folder)
+            logging.info(f"Folder created: {replica_folder}")
 
-        # Removes the existing directory creates a new one
-        shutil.rmtree(replica_folder)
-        shutil.copytree(source_folder, replica_folder)
+        # Copy the missing files in the replica folder from the source folder
+        for root, dirs, files in os.walk(source_folder):
+            for file in files:
+                source_file = os.path.join(root, file)
+                replica_file = os.path.join(
+                    replica_folder,
+                    os.path.relpath(source_file, source_folder)
+                )
+                if not os.path.exists(replica_file):
+                    shutil.copy(source_file, replica_file)
+                    logging.info(
+                        f"File copied: {source_file} -> {replica_file}"
+                    )
+
+        # Remove files in replica folder that don't exist in source folder
+        for root, dirs, files in os.walk(replica_folder):
+            for file in files:
+                replica_file = os.path.join(root, file)
+                source_file = os.path.join(
+                    source_folder,
+                    os.path.relpath(replica_file, replica_folder)
+                )
+                if not os.path.exists(source_file):
+                    os.remove(replica_file)
+                    logging.info(f"File removed: {replica_file}")
     except Exception as e:
-        print(e)
+        logging.error(f"Synchronization failed: {e}")
 
 
 def main():
-    if len(sys.argv) != 4:
-        # Example: python3 folder_sync.py "source_folder" "replica_folder"
-        print('Usage: python3 main.py <source_folder> <replica_folder>')
+    if len(sys.argv) != 5:
+        # Example: python3 folder_sync.py src_folder rep_folder 5 events.log
+        print(
+            'Usage: python3 main.py <source_folder>'
+            '<replica_folder> <interval> <log_file>'
+        )
         sys.exit(1)
 
     source_folder = sys.argv[1]
     replica_folder = sys.argv[2]
     interval = int(sys.argv[3])
+    log_file = sys.argv[4]
 
-    while True:
-        synchronize_folders(source_folder, replica_folder)
-        time.sleep(interval)
+    logger_setup(log_file)
+    logging.info(
+        f"Folder synchronization started: {source_folder} -> {replica_folder}"
+    )
+
+    try:
+        while True:
+            synchronize_folders(source_folder, replica_folder)
+            time.sleep(interval)
+    except KeyboardInterrupt:
+        logging.info("Execution interrupted via CTRL-C")
 
 
 if __name__ == "__main__":
